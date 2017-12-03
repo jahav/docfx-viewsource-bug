95c3edc9e7ff1123c5a852cd7ae60b3df12b0e83
jahav <havlicek.honza@gmail.com>
2017-12-03 23:12:52 +0100

    Add library

---
+++ TestLibrary/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TestLibrary
+{
+    /// <summary>
+    /// This is a test class that has a bad link to the GitHub.
+    /// </summary>
+    public class Class1
+    {
+        /// <summary>
+        /// A method to demonstrate bug.
+        /// </summary>
+        public void Method()
+        {
+
+        }
+    }
+}
